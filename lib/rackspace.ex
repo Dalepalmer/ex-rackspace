d3482a81096c8ab94d621487596063d90211905c
Aaron Ross <superhawk610@gmail.com>
2020-03-16 11:14:17 -0700

    reformat application module

--- lib/rackspace.ex
+++ lib/rackspace.ex
@@ -1,19 +1,9 @@
 defmodule Rackspace do
   use Application
-  require Logger
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
-  def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-    children = [
-      # Define workers and child supervisors to be supervised
-      worker(Rackspace.Config, [])
-    ]
+  import Supervisor.Spec
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Rackspace.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
+  @children [worker(Rackspace.Config, [])]
+  @opts [strategy: :one_for_one, name: Rackspace.Supervisor]
 
+  def start(_type, _args), do: Supervisor.start_link(@children, @opts)
 end
