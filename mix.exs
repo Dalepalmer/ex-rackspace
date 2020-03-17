f4316ef8ef024848958af30af3c912cfad7b63af
Aaron Ross <superhawk610@gmail.com>
2020-03-17 11:01:06 -0700

    bumped version to 2.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rackspace.Deprecated.Mixfile do
   def project do
     [
       app: :rackspace,
-      version: "0.1.0",
+      version: "2.0.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
